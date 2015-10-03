d8bb182e97a5788a41d64e4e288f3ccd636f4c9b
MH-L <922lmh922@gmail.com>
2015-10-03 15:33:54 +0800

    Added destroy method.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -14,9 +14,17 @@ struct Poker {
 
 Poker* deck;
 
+void initialize();
+void shuffleDeck();
+void destroy();
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	initialize();
+	Poker* player1;
+	Poker* player2;
+	player1 = (Poker*)malloc(POKER_SUM / 2 * sizeof(Poker*));
+	player2 = (Poker*)malloc(POKER_SUM / 2 * sizeof(Poker*));
 	std::string userIn;
 	std::cout << "Welcome to Poker Game." << std::endl;
 	std::cout << "Would you like to play first or second?" << std::endl;
@@ -25,11 +33,20 @@ int _tmain(int argc, _TCHAR* argv[])
 	if (userIn.compare("yes") || userIn.compare("no")) {
 		printf("You have succeeded.");
 	}
-	while (true) {
-		std::cout << "Now you get a new card, which is " << std::endl;
-		if (noCardLeft())
-			break;
+	int counter = 0;
+	while (counter < POKER_SUM) {
+		int index = counter / 2;
+		printf("Now you get a new card, which is %s\n", "hehe");
+		if (counter % 2 == 0)
+			*(player1 + index) = *(deck + counter);
+		else
+			*(player2 + index) = *(deck + counter);
+		counter++;
+
 	}
+	delete[] player1;
+	delete[] player2;
+	destroy();
 	getchar();
 	return 0;
 }
@@ -45,10 +62,10 @@ void initialize() {
 	}
 }
 
-bool noCardLeft() {
-
+void shuffleDeck() {
+	// do nothing now.
 }
 
-void shuffleDeck() {
-
+void destroy() {
+	delete[] deck;
 }
